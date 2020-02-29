468f9cbe5a074c8c12734b524921e0f141aea006
Rodolfo da Costa Lacerda <34213284+RodolfoCosta@users.noreply.github.com>
2020-02-29 20:18:42 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,18 @@
+package br.com.eiconTeste.clientesapi;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class Cliente {
+		
+	private Long codigoCliente;
+	private Long numeroControle;	
+	private String nomeProduto;
+	private Long valorUnitario;
+	
+	
+}
